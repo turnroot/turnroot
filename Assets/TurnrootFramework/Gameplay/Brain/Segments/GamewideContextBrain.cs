cda7852fd4f06b80441768ad6170b31a3c8c391c
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2025-12-21 20:17:51 -0600

    Update GamewideContextBrain.cs

--- Assets/TurnrootFramework/Gameplay/Brain/Segments/GamewideContextBrain.cs
+++ Assets/TurnrootFramework/Gameplay/Brain/Segments/GamewideContextBrain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Turnroot.Characters;
 using UnityEngine;
@@ -30,6 +31,11 @@ namespace Turnroot.Gameplay.Brain
         private RosterManager _rosterManager;
         private CharacterPersistence _characterPersistence;
 
+        // Event handlers for EventBus
+        private Action<CharacterInstance> _onCharacterLevelUpHandler;
+        private Action<CharacterInstance> _onCharacterClassChangedHandler;
+        private Action<CharacterInstance, Skill> _onCharacterLearnedSkillHandler;
+
         [Header("Rosters")]
         [SerializeField]
         private List<GenericRoster> _genericRosters = new();
@@ -41,26 +47,41 @@ namespace Turnroot.Gameplay.Brain
 
         protected override void Awake()
         {
-            base.Awake();
+            _brain = GetComponent<Brain>();
+            if (_brain == null)
+            {
+                Debug.LogError($"{GetType().Name}: Brain component not found!");
+                return;
+            }
 
             // Initialize managers
             _rosterManager = new RosterManager(this, _brain);
             _characterPersistence = new CharacterPersistence(GetComponent<LongTermMemory>(), this);
+
+            // Now subscribe to brain events
+            Debug.Log(
+                $"{GetType().Name} Awake - subscribing to brain events with priority {GetSubscriptionPriority()}."
+            );
+            SubscribeToBrainEvents();
         }
 
         protected override void SubscribeToBrainEvents()
         {
             _brain.OnRostersReady += _rosterManager.OnRostersReady;
-            _brain.OnCharacterLevelUp += (c) => _rosterManager.InvalidateCache();
-            _brain.OnCharacterClassChanged += (c) => _rosterManager.InvalidateCache();
-            _brain.OnCharacterLearnedSkill += (c, s) => _rosterManager.InvalidateCache();
+            _onCharacterLevelUpHandler = (c) => _rosterManager.InvalidateCache();
+            _brain.OnCharacterLevelUp += _onCharacterLevelUpHandler;
+            _onCharacterClassChangedHandler = (c) => _rosterManager.InvalidateCache();
+            _brain.OnCharacterClassChanged += _onCharacterClassChangedHandler;
+            _onCharacterLearnedSkillHandler = (c, s) => _rosterManager.InvalidateCache();
+            _brain.OnCharacterLearnedSkill += _onCharacterLearnedSkillHandler;
         }
 
         protected override void UnsubscribeFromBrainEvents()
         {
             _brain.OnRostersReady -= _rosterManager.OnRostersReady;
-            _brain.OnCharacterLevelUp -= (c) => _rosterManager.InvalidateCache();
-            _brain.OnCharacterClassChanged -= (c) => _rosterManager.InvalidateCache();
+            _brain.OnCharacterLevelUp -= _onCharacterLevelUpHandler;
+            _brain.OnCharacterClassChanged -= _onCharacterClassChangedHandler;
+            _brain.OnCharacterLearnedSkill -= _onCharacterLearnedSkillHandler;
         }
 
         public void Start()
