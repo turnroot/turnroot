aa1876176be03b2c6039fa6dcb9effa6ac8edbb5
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2026-01-07 15:51:09 -0600

    Update SineMaterialColors.cs

--- Assets/TurnrootFramework/AbstractScripts/UI/SineMaterialColors.cs
+++ Assets/TurnrootFramework/AbstractScripts/UI/SineMaterialColors.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class SineMaterialColors : MonoBehaviour
 {
     public float Speed = 1.0f;
@@ -8,18 +9,39 @@ public class SineMaterialColors : MonoBehaviour
     public Color startColor;
     public Color endColor;
 
+    private Material _materialInstance;
+    private Renderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+
+        _materialInstance = _renderer.material;
+    }
+
     private void OnDestroy()
     {
-        TargetMaterial.color = startColor;
+        if (_materialInstance != null)
+        {
+            _materialInstance.color = startColor;
+            if (Application.isPlaying)
+            {
+                Destroy(_materialInstance);
+            }
+            else
+            {
+                DestroyImmediate(_materialInstance);
+            }
+        }
     }
 
     private void Update()
     {
-        if (TargetMaterial != null)
+        if (_materialInstance != null)
         {
             float sineValue = (Mathf.Sin(Time.time * Speed) + 1) / 2; // Normalize sine to [0,1]
             Color newColor = Color.Lerp(startColor, endColor, sineValue);
-            TargetMaterial.color = newColor;
+            _materialInstance.color = newColor;
         }
     }
 }
