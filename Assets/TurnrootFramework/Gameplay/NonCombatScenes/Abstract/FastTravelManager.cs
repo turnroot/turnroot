8f491c2ddaada00754487fe0b35337deb811b3a7
Joseph Hansen <josephclaytonhansen@gmail.com>
2026-07-11 12:21:14 -0500

    continue cleanup

--- Assets/TurnrootFramework/Gameplay/NonCombatScenes/Abstract/FastTravelManager.cs
+++ Assets/TurnrootFramework/Gameplay/NonCombatScenes/Abstract/FastTravelManager.cs
@@ -4,31 +4,14 @@ namespace Turnroot.NonCombatScenes.Abstract
 {
     public class FastTravelManager : MonoBehaviour
     {
-        [Tooltip("Unlockable locations that gate whether a fast-travel destination is available.")]
-        public UnlockableWorldLocation[] UnlockableLocations;
-
-        public bool IsLocationAvailable(Transform fastTravelPoint)
+        public bool IsLocationAvailable(UnlockableWorldLocation location)
         {
-            if (fastTravelPoint == null || UnlockableLocations == null)
+            if (location == null || location.fastTravelPoint == null)
             {
                 return false;
             }
 
-            for (int i = 0; i < UnlockableLocations.Length; i++)
-            {
-                var location = UnlockableLocations[i];
-                if (location == null)
-                {
-                    continue;
-                }
-
-                if (location.fastTravelPoint == fastTravelPoint)
-                {
-                    return location.isUnlocked;
-                }
-            }
-
-            return false;
+            return location.isUnlocked;
         }
     }
 }
