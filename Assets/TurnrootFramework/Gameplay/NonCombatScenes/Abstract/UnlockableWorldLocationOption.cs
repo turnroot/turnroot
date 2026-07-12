f8598e237c8ddc26645bb20c1028b5eb2d639dd0
Joseph Hansen <josephclaytonhansen@gmail.com>
2026-07-11 22:35:55 -0500

    UnlockableWorldLocationOption

---
+++ Assets/TurnrootFramework/Gameplay/NonCombatScenes/Abstract/UnlockableWorldLocationOption.cs
@@ -0,0 +1,21 @@
+using System;
+using NaughtyAttributes;
+using Turnroot.Gameplay.NonCombatScenes.Hub;
+using Turnroot.UI;
+using UnityEngine;
+
+namespace Turnroot.NonCombatScenes.Abstract
+{
+    [Serializable]
+    public struct UnlockableWorldLocationOption
+    {
+        public UiChoice Choice;
+
+        [Tooltip("Location name used to update CurrentLocationName on travel.")]
+        public HubSublocationName LocationName;
+        public UnlockableWorldLocation UnlockableLocation;
+
+        [HideInInspector]
+        public bool available;
+    }
+}
